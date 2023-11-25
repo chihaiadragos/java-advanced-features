2267cd06ef3ec95b54317f77c936aa742111521c
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2023-11-25 13:55:03 +0200

    ClassesAndInterfaces: Task 3

---
+++ src/ClassTaxonomy/Coding/CkassesAndInterfaces/Task3/Main.java
@@ -0,0 +1,15 @@
+package ClassTaxonomy.Coding.CkassesAndInterfaces.Task3;
+
+import ClassTaxonomy.Features.InnerClasses.Car;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car("renault", "economy");
+        System.out.println(car1);
+        Car car2 = new Car("BMW", "luxury");
+        System.out.println(car2);
+        Car car3 = new Car("dacia", "turbo");
+        System.out.println(car3);
+    }
+
+}
