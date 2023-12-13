c426ffc8c0edafd2b12b3839887105dc0fde7218
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2023-12-13 20:15:40 +0200

    LeetCode: 1582 fix bugg

--- src/LeetCode/specialPositionsinaBinaryMatrix1582/Solution.java
+++ src/LeetCode/specialPositionsinaBinaryMatrix1582/Solution.java
@@ -2,28 +2,37 @@ package LeetCode.specialPositionsinaBinaryMatrix1582;
 
 public class Solution {
     public static void main(String[] args) {
-        int[][] num = {{1,0,0},{0,0,1},{1,0,0}};
+        int[][] num = {{0, 0}, {0, 0}, {1, 0}};
         System.out.println(numSpecial(num));
     }
+
     public static int numSpecial(int[][] mat) {
+        int count = 0;
         int R = mat.length;
         int C = mat[0].length;
         int[] row = new int[R];
         int[] col = new int[C];
 
-        for (int i=0; i < R; i++) {
-            for (int j = 0; j < R; j++) {
-                if (mat[i][j] ==1) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] == 0) {
                     row[i]++;
-                    col[j]++;
                 }
             }
         }
-        int count = 0;
-        for (int i =0; i < R; i++) {
-            for (int j = 0; j<C; j++) {
-                if (mat[i][j]==1 && row[i]==1 && col[j]==1){
-                    count++;
+        for (int i = 0; i < mat[0].length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                if (mat[j][i] == 0) {
+                    col[i]++;
+                }
+            }
+        }
+        for (int k = 0; k < mat.length; k++) {
+            for (int w = 0; w < mat[0].length; w++){
+                if (mat[k][w] ==1) {
+                    if (row[k]==C-1 && col[w] == R-1) {
+                        count++;
+                    }
                 }
             }
         }
