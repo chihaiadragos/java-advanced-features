c25ed9e9259efac0b12cae8de607d38555de751d
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2023-12-18 18:10:13 +0200

    Enum: HOMEWORK metric converter lambda

--- src/ObjectOrientedProgramming/Coding/EnumCoding/homeWorkMetricConverter/Main.java
+++ src/ObjectOrientedProgramming/Coding/EnumCoding/homeWorkMetricConverter/Main.java
@@ -2,8 +2,10 @@ package ObjectOrientedProgramming.Coding.EnumCoding.homeWorkMetricConverter;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(MetricConverter.convertLengthUnit("M", "KM", 12345));
-        System.out.println(MetricConverter.convertLengthUnit("MM", "KM", 12345));
-        System.out.println(MetricConverter.convertLengthUnit("CM", "KM", 12345));
+        float value = 12345f;
+        String output = "KM";
+        System.out.println(MetricConverter.convertLengthUnit("M", output, value));
+        System.out.println(MetricConverter.convertLengthUnit("MM", output, value));
+        System.out.println(MetricConverter.convertLengthUnit("CM", output, value));
     }
 }
--- src/ObjectOrientedProgramming/Coding/EnumCoding/homeWorkMetricConverter/MetricConverter.java
+++ src/ObjectOrientedProgramming/Coding/EnumCoding/homeWorkMetricConverter/MetricConverter.java
@@ -3,78 +3,18 @@ package ObjectOrientedProgramming.Coding.EnumCoding.homeWorkMetricConverter;
 
 
 public enum MetricConverter {
-    KM_M("KM", "M", new Converter() {
-        @Override
-        public float convert(float lengthValueIn) {
-            return lengthValueIn * 1000;
-        }
-    }),
-    KM_CM("KM", "CM", new Converter() {
-        @Override
-        public float convert(float lengthValueIn) {
-            return lengthValueIn * 100000;
-        }
-    }),
-    KM_MM("KM", "MM", new Converter() {
-        @Override
-        public float convert(float lengthValueIn) {
-            return lengthValueIn * 1000000;
-        }
-    }),
-    M_KM("M", "KM", new Converter() {
-        @Override
-        public float convert(float lengthValueIn) {
-            return lengthValueIn / 1000;
-        }
-    }),
-    M_CM("M", "CM", new Converter() {
-        @Override
-        public float convert(float lengthValueIn) {
-            return lengthValueIn * 100;
-        }
-    }),
-    M_MM("M", "MM", new Converter() {
-        @Override
-        public float convert(float lengthValueIn) {
-            return lengthValueIn * 100;
-        }
-    }),
-    CM_KM("CM", "KM", new Converter() {
-        @Override
-        public float convert(float lengthValueIn) {
-            return lengthValueIn / 10000;
-        }
-    }),
-    CM_M("CM", "M", new Converter() {
-        @Override
-        public float convert(float lengthValueIn) {
-            return lengthValueIn / 100;
-        }
-    }),
-    CM_MM("CM", "MM", new Converter() {
-        @Override
-        public float convert(float lengthValueIn) {
-            return lengthValueIn * 10;
-        }
-    }),
-    MM_KM("MM", "KM", new Converter() {
-        @Override
-        public float convert(float lengthValueIn) {
-            return lengthValueIn / 1000000;
-        }
-    }),
-    MM_M("MM", "M", new Converter() {
-        @Override
-        public float convert(float lengthValueIn) {
-            return lengthValueIn / 1000;
-        }
-    }),
-    MM_CM("MM", "CM", new Converter() {
-        @Override
-        public float convert(float lengthValueIn) {
-            return lengthValueIn / 10;
-        }
-    });
+    KM_M("KM", "M", lengthValueIn -> lengthValueIn * 1000),
+    KM_CM("KM", "CM", lengthValueIn -> lengthValueIn * 100000),
+    KM_MM("KM", "MM", lengthValueIn -> lengthValueIn * 1000000),
+    M_KM("M", "KM", lengthValueIn -> lengthValueIn / 1000),
+    M_CM("M", "CM", lengthValueIn -> lengthValueIn * 100),
+    M_MM("M", "MM", lengthValueIn -> lengthValueIn * 100),
+    CM_KM("CM", "KM", lengthValueIn -> lengthValueIn / 10000),
+    CM_M("CM", "M", lengthValueIn -> lengthValueIn / 100),
+    CM_MM("CM", "MM", lengthValueIn -> lengthValueIn * 10),
+    MM_KM("MM", "KM", lengthValueIn -> lengthValueIn / 1000000),
+    MM_M("MM", "M", lengthValueIn -> lengthValueIn / 1000),
+    MM_CM("MM", "CM", lengthValueIn -> lengthValueIn / 10);
 
     final String inputLengthUnit;
     final String outputLengthUnit;
