f3ff94b0f8cf000308802f2d70a33827d3305d7e
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2023-12-18 18:05:48 +0200

    Enum: HOMEWORK metric converter refactor

--- src/ObjectOrientedProgramming/Coding/EnumCoding/homeWorkMetricConverter/Main.java
+++ src/ObjectOrientedProgramming/Coding/EnumCoding/homeWorkMetricConverter/Main.java
@@ -3,5 +3,7 @@ package ObjectOrientedProgramming.Coding.EnumCoding.homeWorkMetricConverter;
 public class Main {
     public static void main(String[] args) {
         System.out.println(MetricConverter.convertLengthUnit("M", "KM", 12345));
+        System.out.println(MetricConverter.convertLengthUnit("MM", "KM", 12345));
+        System.out.println(MetricConverter.convertLengthUnit("CM", "KM", 12345));
     }
 }
