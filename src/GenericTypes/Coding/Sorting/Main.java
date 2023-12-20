e1360fe0d0cc505fa65f78e70f0f088037755a93
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2023-12-20 21:01:44 +0200

    GenericTypes: stream() sort

--- src/GenericTypes/Coding/Sorting/Main.java
+++ src/GenericTypes/Coding/Sorting/Main.java
@@ -1,5 +1,9 @@
 package GenericTypes.Coding.Sorting;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
         Integer[] num1 = {5, 1, 6, 9, 3, 2};
@@ -8,5 +12,14 @@ public class Main {
 
         String[] num2 = {"baboon", "Star", "OCA", "123password", "New York", "1New York", "crayon"};
         Sort.sortingList(num2);
-     }
+        System.out.println("==========================");
+
+        List<Integer> num3 = Arrays.asList(5, 1, 6, 9, 3, 2);
+        System.out.println(num3.stream().sorted().collect(Collectors.toList()));
+        System.out.println("==========================");
+
+        List<String> num4 = Arrays.asList("baboon", "Star", "OCA", "123password", "New York", "1New York", "crayon");
+        System.out.println(num4.stream().sorted().collect(Collectors.toList()));
+        System.out.println("==========================");
+    }
 }
