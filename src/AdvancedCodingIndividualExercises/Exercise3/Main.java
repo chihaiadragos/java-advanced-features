4890f9ad0f15c92374f6644ee95861b65b34d26a
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2023-12-28 19:59:00 +0200

    AdvancedCodingIndividualExercises: Exercise3 optional.isPresent test

--- src/AdvancedCodingIndividualExercises/Exercise3/Main.java
+++ src/AdvancedCodingIndividualExercises/Exercise3/Main.java
@@ -1,13 +1,16 @@
 package AdvancedCodingIndividualExercises.Exercise3;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.OptionalDouble;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1, 2, 3);
-        System.out.println(average(list));
+        List<Integer> list1 = Arrays.asList(1, 2, 3);
+        List<Integer> list2 = new ArrayList<>();
+        System.out.println(average(list1));
+        System.out.println(average(list2));
     }
     public static double average(List<Integer> list) {
         OptionalDouble optionalDouble = list.stream().mapToInt(i -> i).average();
