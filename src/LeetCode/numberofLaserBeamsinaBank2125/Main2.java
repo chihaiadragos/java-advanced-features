783b4534a3076303d01449d0d8d254d290e476d1
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2024-01-04 18:02:07 +0200

    FizzBuzz with IntStream interface

--- src/LeetCode/numberofLaserBeamsinaBank2125/Main2.java
+++ src/LeetCode/numberofLaserBeamsinaBank2125/Main2.java
@@ -1,7 +1,12 @@
 package LeetCode.numberofLaserBeamsinaBank2125;
 
+import java.util.stream.IntStream;
+
 public class Main2 {
     public static void main(String[] args) {
-
+        int n = 100;
+        IntStream.range(1, n)
+                .mapToObj(i -> i % 3 == 0 ? (i % 5 == 0 ? "FizzBuzz " : "Fizz ") : (i % 5 == 0 ? "Buzz " : i + " "))
+                .forEach(System.out::print);
     }
 }
