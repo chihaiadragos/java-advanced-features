14c5e5a081ba63d2f4cae79f374e100e09bec765
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2024-01-12 16:40:09 +0200

    LeetCode: 1704 reformat code

--- src/LeetCode/determineIfStringHalvesAreAlike1704/Solution.java
+++ src/LeetCode/determineIfStringHalvesAreAlike1704/Solution.java
@@ -9,14 +9,16 @@ public class Solution {
     }
     public static boolean halvesAreAlike(String s) {
         char[] ch = s.toCharArray();
-        int left=0,right=ch.length-1;
+        int left = 0;
+        int right = ch.length - 1;
 
-        int lcount=0,rcount=0;
-        Set vowels=Set.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
+        int lcount = 0;
+        int rcount = 0;
+        Set vowels = Set.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
 
-        while(left<right){
-            lcount += vowels.contains(ch[left])?1:0;
-            rcount += vowels.contains(ch[right]) ? 1:0;
+        while(left < right){
+            lcount += vowels.contains(ch[left]) ? 1 : 0;
+            rcount += vowels.contains(ch[right]) ? 1 : 0;
             left++;
             right--;
         }
