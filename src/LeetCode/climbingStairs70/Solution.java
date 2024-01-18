8a507797926edcdfbcd302fa1d2e2d9fc948d1d1
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2024-01-18 19:49:57 +0200

    LeetCode: 70

---
+++ src/LeetCode/climbingStairs70/Solution.java
@@ -0,0 +1,22 @@
+package LeetCode.climbingStairs70;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(2);
+        System.out.println(3);
+    }
+    public int climbStairs(int n) {
+        if (n == 1)
+            return 1;
+
+        int arr[] = new int[n+1];
+        arr[1] = 1;
+        arr[2] = 2;
+
+        for (int i = 3; i <= n; i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+
+        return arr[n];
+    }
+}
