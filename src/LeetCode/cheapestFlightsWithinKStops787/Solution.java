4f4129172297a7a2bb62b108a6595bf2d3c39a65
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2024-02-23 21:20:29 +0200

    LeetCode: 787

---
+++ src/LeetCode/cheapestFlightsWithinKStops787/Solution.java
@@ -0,0 +1,45 @@
+package LeetCode.cheapestFlightsWithinKStops787;
+
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(findCheapestPrice(4, new int[][]{{0,1,100},{1,2,100},{2,0,100},{1,3,600},{2,3,200}}, 0, 3, 1));
+    }
+    public static int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        Map<Integer, List<int[]>> adj = new HashMap<>();
+        for (int[] flight : flights) {
+            adj.computeIfAbsent(flight[0], key -> new ArrayList<>()).add(new int[] {flight[1], flight[2]});
+        }
+
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] {src, 0});
+        int stops = 0;
+
+        while (!q.isEmpty() && stops <= k) {
+            int sz = q.size();
+            while (sz-- > 0) {
+                int[] curr = q.poll();
+                int node = curr[0];
+                int distance = curr[1];
+
+                if (!adj.containsKey(node)) continue;
+
+                for (int[] next : adj.get(node)) {
+                    int neighbour = next[0];
+                    int price = next[1];
+                    if (price + distance >= dist[neighbour]) continue;
+                    dist[neighbour] = price + distance;
+                    q.offer(new int[] {neighbour, dist[neighbour]});
+                }
+            }
+            stops++;
+        }
+
+        return dist[dst] == Integer.MAX_VALUE ? -1 : dist[dst];
+    }
+}
