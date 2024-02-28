e3c37f85dd38abd630cc8dd7811dcd134edac9c9
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2024-02-28 21:57:15 +0200

    LeetCode: 513

---
+++ src/LeetCode/findBottomLeftTreeValue513/Solution.java
@@ -0,0 +1,45 @@
+package LeetCode.findBottomLeftTreeValue513;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        int last = 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int count = q.size();
+            for (int i = 0; i < count; i++) {
+                TreeNode curr = q.poll();
+                if (i == 0)
+                    last = curr.val;  // last leftMost val
+                if (curr.left != null)
+                    q.add(curr.left);
+                if (curr.right != null)
+                    q.add(curr.right);
+            }
+        }
+        return last;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
