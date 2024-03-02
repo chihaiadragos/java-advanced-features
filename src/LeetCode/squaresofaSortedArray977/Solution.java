7b50f37086121d6d85cbd3a205b5b51035149cbf
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2024-03-02 19:07:07 +0200

    LeetCode: 977

---
+++ src/LeetCode/squaresofaSortedArray977/Solution.java
@@ -0,0 +1,57 @@
+package LeetCode.squaresofaSortedArray977;
+
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sortedSquares(new int[]{-4, -1, 0, 3, 10})));
+        System.out.println(Arrays.toString(sortedSquares(new int[]{-7, -3, 2, 3, 11})));
+    }
+    public static int getMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void countSort(int[] arr, int exp) {
+        int[] output = new int[arr.length];
+        int[] count = new int[10];
+
+        for (int i = 0; i < arr.length; i++) {
+            count[(arr[i] / exp) % 10]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = output[i];
+        }
+    }
+
+    public static void radixSort(int[] arr) {
+        int max = getMax(arr);
+
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countSort(arr, exp);
+        }
+    }
+
+    public static int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * nums[i];
+        }
+        radixSort(nums);
+        return nums;
+    }
+}
