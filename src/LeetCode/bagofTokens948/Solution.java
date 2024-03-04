3a0040356de7976ec1f98b24d85bf7cc10a05578
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2024-03-04 19:09:58 +0200

    LeetCode: 948

---
+++ src/LeetCode/bagofTokens948/Solution.java
@@ -0,0 +1,33 @@
+package LeetCode.bagofTokens948;
+
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(bagOfTokensScore(new int[]{100}, 50));
+        System.out.println(bagOfTokensScore(new int[]{200,100}, 150));
+    }
+    public static int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int s = 0;
+        int maxi = 0;
+        int l = 0, r = tokens.length - 1;
+
+        while (l <= r) {
+            if (power >= tokens[l]) {
+                power -= tokens[l];
+                s++;
+                l++;
+                maxi = Math.max(maxi, s);
+            } else if (s > 0) {
+                power += tokens[r];
+                s--;
+                r--;
+            } else {
+                break;
+            }
+        }
+
+        return maxi;
+    }
+}
