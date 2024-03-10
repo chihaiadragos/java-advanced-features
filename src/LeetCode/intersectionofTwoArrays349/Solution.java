872d32d602b5e84dfc1390ddffffa55a172e12f9
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2024-03-10 16:56:04 +0200

    LeetCode: 349

---
+++ src/LeetCode/intersectionofTwoArrays349/Solution.java
@@ -0,0 +1,29 @@
+package LeetCode.intersectionofTwoArrays349;
+
+import java.util.*;
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(intersection(new int[]{1, 2, 2, 1}, new int[]{2, 2})));
+    }
+    public static int[] intersection(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> resultList = new ArrayList<>();
+        for (int num : nums2) {
+            if (map.containsKey(num)) {
+                resultList.add(num);
+                map.remove(num);
+            }
+        }
+
+        int[] result = new int[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+            result[i] = resultList.get(i);
+        }
+        return result;
+    }
+}
