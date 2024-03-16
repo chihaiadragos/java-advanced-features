9c616fbf073327b6637bea7cba3aa6c80cc36902
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2024-03-16 21:39:46 +0200

    LeetCode: 525

---
+++ src/LeetCode/contiguousArray525/Solution.java
@@ -0,0 +1,28 @@
+package LeetCode.contiguousArray525;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(findMaxLength(new int[]{0,1}));
+        System.out.println(findMaxLength(new int[]{0,1,0}));
+    }
+    public static int findMaxLength(int[] nums) {
+        int n = nums.length;
+        Map<Integer, Integer> mp = new HashMap<>();
+        int sum = 0;
+        int subArrayLength = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i] == 0 ? -1 : 1;
+            if (sum == 0) {
+                subArrayLength = i + 1;
+            } else if (mp.containsKey(sum)) {
+                subArrayLength = Math.max(subArrayLength, i - mp.get(sum));
+            } else {
+                mp.put(sum, i);
+            }
+        }
+        return subArrayLength;
+    }
+}
