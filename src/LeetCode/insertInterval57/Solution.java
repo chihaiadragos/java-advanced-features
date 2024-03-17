c6eb38a0538d5451bdb6a5d0f2e4aaa0b6ec438e
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2024-03-17 18:40:16 +0200

    LeetCode: 57

---
+++ src/LeetCode/insertInterval57/Solution.java
@@ -0,0 +1,44 @@
+package LeetCode.insertInterval57;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(insert(new int[][]{{1, 3}, {6, 9}}, new int[]{2, 5})));
+    }
+    public static int[][] insert(int[][] intervals, int[] newInterval) {
+        List<int[]> ans = new ArrayList<>();
+        int start = newInterval[0], end = newInterval[1];
+        boolean inserted = false;
+
+        for (int[] inv : intervals) {
+            int cstart = inv[0], cend = inv[1];
+
+            if (cend < start || inserted) {
+                ans.add(new int[]{cstart, cend});
+                continue;
+            }
+
+            start = Math.min(start, cstart);
+            if (end < cstart) {
+                ans.add(new int[]{start, end});
+                ans.add(new int[]{cstart, cend});
+                inserted = true;
+                continue;
+            }
+
+            if (end <= cend) {
+                ans.add(new int[]{start, cend});
+                inserted = true;
+            }
+        }
+
+        if (!inserted) {
+            ans.add(new int[]{start, end});
+        }
+
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
