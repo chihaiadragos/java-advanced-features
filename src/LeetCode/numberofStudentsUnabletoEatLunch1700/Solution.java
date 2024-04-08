f0ab632c78202baa308a238994a1175a12e14fca
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2024-04-08 20:25:07 +0300

    LeetCode: 1700

---
+++ src/LeetCode/numberofStudentsUnabletoEatLunch1700/Solution.java
@@ -0,0 +1,20 @@
+package LeetCode.numberofStudentsUnabletoEatLunch1700;
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(countStudents(new int[]{1,1,0,0}, new int[]{0,1,0,1}));
+    }
+    public static int countStudents(int[] students, int[] sandwiches) {
+        int[] counts = new int[2];
+        for (int student : students) counts[student]++;
+
+        int remaining = sandwiches.length;
+        for (int sandwich : sandwiches) {
+            if (counts[sandwich] == 0) break;
+            if (remaining-- == 0) break;
+            counts[sandwich]--;
+        }
+
+        return remaining;
+    }
+}
