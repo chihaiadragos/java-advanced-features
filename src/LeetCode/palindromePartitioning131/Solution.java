4c3e6090bf1562c00cf81fd884c00bb0548c652f
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2024-05-24 23:30:00 +0300

    LeetCode: 131

---
+++ src/LeetCode/palindromePartitioning131/Solution.java
@@ -0,0 +1,44 @@
+package LeetCode.palindromePartitioning131;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(partition("aab"));
+    }
+    public static List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        backtrack(s, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private static void backtrack(String s, int start, List<String> path, List<List<String>> result) {
+
+        if (start == s.length()) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int end = start + 1; end <= s.length(); end++) {
+
+            if (isPalindrome(s, start, end - 1)) {
+                path.add(s.substring(start, end));
+
+                backtrack(s, end, path, result);
+
+                path.remove(path.size() - 1);
+            }
+        }
+    }
+
+    private static boolean isPalindrome(String s, int left, int right) {
+
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
