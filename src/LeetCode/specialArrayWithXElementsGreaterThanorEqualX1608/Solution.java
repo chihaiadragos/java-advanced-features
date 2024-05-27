04539bda12f9f43610a2752fa906a6b6f33003c0
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2024-05-27 22:02:05 +0300

    LeetCode: 1608

---
+++ src/LeetCode/specialArrayWithXElementsGreaterThanorEqualX1608/Solution.java
@@ -0,0 +1,23 @@
+package LeetCode.specialArrayWithXElementsGreaterThanorEqualX1608;
+
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(specialArray(new int[]{2,5}));
+    }
+    public static int specialArray(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        if (nums[0] >= n) return n;
+
+        for (int i = 1; i <= n; i++) {
+            if (nums[n - i] >= i && (n - i - 1 < 0 || nums[n - i - 1] < i)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
