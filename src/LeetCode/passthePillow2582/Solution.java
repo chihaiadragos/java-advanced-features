5d3ecde9193829cd045e0840aeac257adac88360
dragoschihaia <dragos.chihaia.gabriel@gmail.com>
2024-07-06 21:48:40 +0300

    Leetcode: 2582

---
+++ src/LeetCode/passthePillow2582/Solution.java
@@ -0,0 +1,11 @@
+package LeetCode.passthePillow2582;
+
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(passThePillow(4,5));
+    }
+    public static int passThePillow(int n, int time) {
+        int chunks = time / (n - 1);
+        return chunks % 2 == 0 ? (time % (n - 1) + 1) : (n - time % (n - 1));
+    }
+}
